4faa34ead73e1a104082bc455f8ce42d24f64262
Honghao Zhang <zhh358@gmail.com>
2014-08-30 16:06:46 -0400

    initial commit

---
+++ UW-Quest/ZHHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZHHAppDelegate.h
+//  UW-Quest
+//
+//  Created by Honghao on 8/29/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
