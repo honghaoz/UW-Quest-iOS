2043db617d3826e478db9e9df5eb47173f84d076
Honghao Zhang <zhh358@gmail.com>
2014-09-22 00:07:30 -0400

    added address cell

---
+++ UW Quest/Controller/PersonalInformation/AddressCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AddressCollectionViewCell.swift
+//  UW Quest
+//
+//  Created by Honghao on 9/22/14.
+//  Copyright (c) 2014 Honghao. All rights reserved.
+//
+
+import UIKit
+
+class AddressCollectionViewCell: UICollectionViewCell {
+    
+}
