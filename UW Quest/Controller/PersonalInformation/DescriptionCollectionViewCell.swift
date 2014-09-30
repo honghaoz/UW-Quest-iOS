c1902f68b8cc286e0b08d355fe5d083b6c4848d3
Honghao Zhang <zhh358@gmail.com>
2014-09-30 00:40:10 -0400

    added email and description cells

---
+++ UW Quest/Controller/PersonalInformation/DescriptionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DescriptionCollectionViewCell.swift
+//  UW Quest
+//
+//  Created by Honghao Zhang on 2014-09-30.
+//  Copyright (c) 2014 Honghao. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UW Quest/Controller/PersonalInformation/EmailCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EmailCollectionViewCell.swift
+//  UW Quest
+//
+//  Created by Honghao Zhang on 2014-09-29.
+//  Copyright (c) 2014 Honghao. All rights reserved.
+//
+
+import UIKit
+
+class EmailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
