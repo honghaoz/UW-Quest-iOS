413c4a41e07a7f6c5fcd092fe51a8bdb641c4be1
Honghao Zhang <zhh358@gmail.com>
2014-10-26 13:37:44 -0400

    Updated Menu view controller

---
+++ UW Quest/Controller/MenuTitleCell.swift
@@ -0,0 +1,39 @@
+//
+//  MenuTitleCell.swift
+//  UW Quest
+//
+//  Created by Honghao Zhang on 2014-10-26.
+//  Copyright (c) 2014 Honghao. All rights reserved.
+//
+
+import UIKit
+
+class MenuTitleCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let kAnimationDuration = 0.4
+    let titleSelectedColor =  UIColor(white: 1.0, alpha: 0.8)
+    let titleNormalColor = UIColor(white: 1.0, alpha: 0.5)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.clearColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            UIView.animateWithDuration(kAnimationDuration, animations: { () -> Void in
+                self.titleLabel.textColor = self.titleSelectedColor
+                self.backgroundColor = UIColor(white: 0.0, alpha: 0.2)
+            })
+        } else {
+            UIView.animateWithDuration(kAnimationDuration, animations: { () -> Void in
+                self.titleLabel.textColor = self.titleNormalColor
+                self.backgroundColor = UIColor.clearColor()
+            })
+        }
+    }
+
+}
--- UW Quest/Controller/MenuViewController.swift
+++ UW Quest/Controller/MenuViewController.swift
@@ -8,20 +8,24 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var footerView: UIView!
+    
+    let kCellIdentifier = "Title"
     
     let backgroundColor = UIColor(red:0.16, green:0.2, blue:0.24, alpha:1)
-    let titleColor = UIColor(red:0.31, green:0.34, blue:0.36, alpha:1)
+    let titleColor = UIColor(white: 1.0, alpha: 0.7)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.headerView.backgroundColor = backgroundColor
-        self.tableView.backgroundColor = backgroundColor
-        self.tableView.separatorStyle = .None
+        self.view.backgroundColor = backgroundColor
+        self.headerView.backgroundColor = UIColor.clearColor()
+        self.tableView.backgroundColor = UIColor.clearColor()
+        self.footerView.backgroundColor = UIColor.clearColor()
         usernameLabel.text = Locator.sharedLocator.user.username
         usernameLabel.textColor = titleColor
     }
@@ -34,5 +38,33 @@ class MenuViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation: UIStatusBarAnimation.Slide)
+    }
+    
+    // MARK: - TableView data source
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var titleCell: MenuTitleCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as MenuTitleCell
+        switch indexPath.row {
+        case 0:
+            titleCell.titleLabel.text = "News"
+        case 1:
+            titleCell.titleLabel.text = "My Academics"
+        case 2:
+            titleCell.titleLabel.text = "My Class Schedule"
+        case 3:
+            titleCell.titleLabel.text = "Search for Classes"
+        case 4:
+            titleCell.titleLabel.text = "Personal Information"
+        default:
+            assert(false, "Wrong row number")
+        }
+        return titleCell
     }
 }
